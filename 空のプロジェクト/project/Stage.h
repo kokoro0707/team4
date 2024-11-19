1c7c1919e496513173891ce13700c79c0c6ab61b
Okamoto-Sakuya <sdt2495gp0004@edu.sca.ac.jp>
2024-11-19 18:38:43 +0900

    Stage追加

---
+++ 空のプロジェクト/project/Stage.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Library/gameObject.h"
+#include "Source/Vector2.h"
+
+class Stage : public GameObject {
+public:
+	Stage();
+	~Stage();
+	void Draw() override;
+	int IsWallRight(VECTOR2 pos);
+	int IsWallDown(VECTOR2 pos);
+	int IsWallLeft(VECTOR2 pos);
+	int IsWallUp(VECTOR2 pos);
+	bool IsGoal(VECTOR2 pos);
+	int hImage;
+
+	int scroll;
+};
