29c2c7774352fa609cc782108187c298b1560f32
koko <>
2024-12-12 12:26:22 +0900

    まつぷなおした

--- 空のプロジェクト/project/Source/Stage.cpp
+++ 空のプロジェクト/project/Source/Stage.cpp
@@ -45,7 +45,7 @@ void Stage::Draw()
 		for (int i = 0; i < WIDTH; i++) {
 			int x = i * 60 + 100;
 			if (map[j][i] == 1) {
-				DrawRectGraph(x, y, 0, 76, 65, 65, hImage, TRUE);
+				DrawRectGraph(x, y, 0, 64, 64, 64, hImage, TRUE);
 			}
 		}
 	}
