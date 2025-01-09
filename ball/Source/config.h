9cf7617d22f72317597a3863f9edf8142592381d
Takahashi-Souma <sdt2495gp0010@edu.sca.ac.jp>
2025-01-09 10:00:03 +0900

    ballの方のファイルだと弾の当たり判定でます

---
+++ ball/Source/config.h
@@ -0,0 +1,6 @@
+#pragma once
+static const int SCREEN_WIDTH = 1980;
+static const int SCREEN_HEIGHT = 1080;
+static const BOOL WINDOW_MODE = FALSE;
+static const char* WINDOW_NAME = "Sword Bout";
+static const float WINDOW_EXTEND = 1.0f;
